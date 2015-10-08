5af87a41c2795e1a05c5fef44d587d5c8b23a27c
Zach Steed <zsteed21@gmail.com>
2015-10-08 15:10:58 -0600

    Completed Food Tracker

---
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,28 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by user on 10/1/15.
+//  Copyright © 2015 NobleS. All rights reserved.
+//
+
+import XCTest
+
+class FoodTrackerTests: XCTestCase {
+    
+    // MARK: FoodTracker Tests
+    
+    // Tests to confirm that the Meal initializer returns nil when no name or a negative rating is provided.
+    func testMealInitialization() {
+        // Success case.
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        
+        // Failure cases.
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        
+        let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+        XCTAssertNil(badRating)
+    }
+}
